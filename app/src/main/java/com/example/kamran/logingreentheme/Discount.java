1a2a8711d2f573233154a4dc566b2ae2b5940446
Voutsas <voutsf@gmail.com>
2017-12-14 00:42:17 +0200

    Complete Upload Discount and publish in Realtime Database

--- app/src/main/java/com/example/kamran/logingreentheme/Discount.java
+++ app/src/main/java/com/example/kamran/logingreentheme/Discount.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,12 +15,16 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 public class Discount extends AppCompatActivity {
     EditText productnametxt,pricetxt,discounttxt,infotxt;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
     Button publishbtn;
     //Camera components
     private Button picturebtn;
@@ -39,6 +44,8 @@ public class Discount extends AppCompatActivity {
         publishbtn = (Button) findViewById(R.id.publish);
         mStorage = FirebaseStorage.getInstance().getReference();
         picturebtn = (Button) findViewById(R.id.pictureupl);
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        databaseReference=firebaseDatabase.getReferenceFromUrl("https://tloffersfinder.firebaseio.com/Discount");
         picturepr = (ImageView) findViewById(R.id.picproduct);
         picturebtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,7 +55,29 @@ public class Discount extends AppCompatActivity {
                 startActivityForResult(inten,GALLERY_INTENT);
             }
         });
+        publishbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adddiscount();
 
+            }
+        });
+
+    }
+    private void adddiscount(){
+        String productname = productnametxt.getText().toString().trim();
+        String price = pricetxt.getText().toString().trim();
+        String discount = discounttxt.getText().toString().trim();
+        String descript = infotxt.getText().toString().trim();
+        if(!TextUtils.isEmpty(productname)){
+            String id = databaseReference.push().getKey();
+            Shops shops = new Shops(id,productname,price,discount,descript);
+
+            databaseReference.child(id).setValue(shops);
+            Toast.makeText(this,"Επιτυχής εγγραφή προσφοράς",Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this,"Πληκτρολογήστε όνομα προσφοράς!",Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/example/kamran/logingreentheme/ShopsList.java
@@ -0,0 +1,12 @@
+package com.example.kamran.logingreentheme;
+
+/**
+ * Created by Voutsas on 13/12/2017.
+ */
+
+public class ShopsList {
+    String shopname;
+    String category;
+    String location;
+    String number;
+}
